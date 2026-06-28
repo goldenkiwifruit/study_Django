8f5d549c523f4bc7eed3dffcb1b54b9e269de04b
goldenkiwifruit <spongesoccer9@gmail.com>
2026-06-28 13:18:38 +0900

    Todo作成画面を追加

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('list/', views.TodoListView.as_view(), name='todo_list'),
     path('<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('new/', views.TodocreateView.as_view(), name='todo_create'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,6 @@
 from . import models
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TodoListView(ListView):
@@ -11,3 +12,9 @@ class TodoDetailView(DetailView):
     model = models.Todo
     templete_name = 'todoapp/todo_detail.html'
     context_object_name = 'todo'
+
+class TodocreateView(CreateView):
+    model = models.Todo
+    template_name = 'todoapp/todo_create.html'
+    fields = ['title', 'memo', 'completed']
+    success_url = reverse_lazy('todo_list')
